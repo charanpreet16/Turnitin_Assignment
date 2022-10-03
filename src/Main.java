284a0ac422143a1c28d87aed3dc23741a1ec69f3
charan <charanpreetflora@gmail.com>
2022-10-03 13:23:09 -0400

    Code changes to create a word frequency map
    Code to create a word frequency map and display the word frequency in descending order

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static Map<String, Integer> getWordFrequency(String phrase){
+        Map<String, Integer> wordsMap = new HashMap<>();
+        String[] wordsInPhrase = phrase.split(" ");
+        for(String word : wordsInPhrase){
+            if(!wordsMap.containsKey(word)){
+                wordsMap.put(word,1);
+            } else {
+                wordsMap.put(word, wordsMap.get(word) + 1);
+                }
+            }
+        Map<String,Integer> wordFrequencyMap = wordsMap.entrySet()
+                                                        .stream()
+                                                         .sorted((w1,w2) -> w2.getValue().compareTo(w1.getValue())).collect(Collectors.toMap(
+                                                                 Map.Entry::getKey,
+                                                                    Map.Entry::getValue,
+                        (w1,w2) -> w1,LinkedHashMap::new));
+
+        return wordFrequencyMap;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getWordFrequency("the cat is out of the bag"));
+    }
+}
